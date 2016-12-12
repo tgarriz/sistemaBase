eb182ee84d2c0ec8c3e5a8d1f695021196dd4263
root <root@ubuntu-xenial.localdomain>
2016-12-12 17:03:17 +0000

    Instalacion de Sistema Base

---
+++ .bash_history
@@ -0,0 +1,48 @@
+cd
+vim prueba.sh
+chmod +x prueba.sh 
+./prueba.sh 
+vim prueba.sh 
+psql -U postgres prueba.sh 
+vim /etc/postgresql/9.5/main/pg_hba.conf 
+service postgesql restart
+service postgresql restart
+psql -U postgres prueba.sh 
+psql -u postgres
+psql -U postgres
+service postgresql restart
+vim /etc/postgresql/9.5/main/pg_hba.conf 
+service postgresql restart
+psql -U postgres
+vim prueba.sh 
+createdb pepe
+vim prueba.sh 
+./prueba.sh 
+vim /etc/postgresql/9.5/main/pg_hba.conf 
+service postgresql restart
+cp /etc/postgresql/9.5/main/pg_hba.conf ./
+cp /etc/postgresql/9.5/main/postgresql.conf 
+cp /etc/postgresql/9.5/main/postgresql.conf 
+vim prueba.sh 
+ls
+cat pg_hba.conf 
+cat pg_hba.conf > pg_hba_sinpass.conf 
+vim pg_hba_sinpass.conf 
+vim pg_hba_sinpass.conf ls -l
+cp pg_hba_sinpass.conf pg_hbaconpass.conf
+vim pg_hba.conf 
+cat pg_hba_sinpass.conf > pg_hba.conf 
+vim pg_hba.conf 
+ls
+rm pg_hba.conf 
+ls
+mv pg_hbaconpass.conf pg_hba_conpass.conf 
+vim /var/lib/tomcat8/conf/tomcat-users.xml 
+service tomcat8 restart
+cp /var/lib/tomcat8/conf/tomcat-users.xml ./
+vim tomcat-users.xml 
+apt-get install git
+vim /etc/default/tomcat8 
+cp /etc/default/tomcat8 ./
+service tomcat8 restart
+reboot
---
+++ todo.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+apt-get update &&  apt-get -y upgrade
+apt-get install -y nmap vim curl php7.0-pgsql php7.0-fpm unzip gdal-bin python-gdal software-properties-common python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-set-default
+apt-get install -y tomcat8 tomcat8-admin tomcat8-user postgresql-9.5 postgresql-9.5-postgis-2.2 postgresql-9.5-postgis-scripts
+wget http://downloads.sourceforge.net/project/geoserver/GeoServer/2.10.0/geoserver-2.10.0-war.zip
+unzip geoserver-2.10.0-war.zip
+cp geoserver.war /var/lib/tomcat8/webapps/
+apt-get update &&  apt-get -y upgrade
+apt-get autoremove
+cat postgresql.conf > /etc/postgresql/9.5/main/postgresql.conf
+cat pg_hba_sinpass.conf > /etc/postgresql/9.5/main/pg_hba.conf
+service postgresql restart
+psql -U postgres -c "create database rafamgis;"
+psql -U postgres -d rafamgis -c "create extension postgis;"
+psql -U postgres -d rafamgis -c "alter user postgres with password 'rafam2016';"
+cat pg_hba_conpass.conf > /etc/postgresql/9.5/main/pg_hba.conf
+cat tomcat-users.xml > /var/lib/tomcat8/conf/tomcat-users.xml
+cat tomcat8 > /etc/default/tomcat8
+service postgresql restart
+service tomcat8 restart
+echo "Si ya tiene instalado y configurado nginx termine el script con Ctrl-c o Ctrl-z"
+echo "de lo contrario presione enter"
+read
+apt-get install nginx
+cat default_nginx-php > /etc/nginx/sites-available/default
+service nginx restart
